56a3133bca83b3c37e12b34bf84958d16e58d0a6
Teri11 <augustineteri157@gmail.com>
2022-03-13 12:14:12 +0300

    added welcoming email message

---
+++ app/email.py
@@ -0,0 +1,10 @@
+from flask_mail import Message
+from flask import render_template
+from . import email
+
+def mail_message(subject,template,to,**kwargs):
+  sender_email = 'augustineteri157@gmail.com'
+  email = Message(subject, sender=sender_email, recipients=[to])
+  email.body= render_template(template + ".txt",**kwargs)
+  email.html = render_template(template + ".html",**kwargs)
+  mail.send(email)
