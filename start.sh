1cdf65270a1ba4996ead3193afffbe994c2b80cb
Teri11 <augustineteri157@gmail.com>
2022-03-13 10:35:58 +0300

    added export URL

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='mynameisaugstine'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/blog'
+export MAIL_USERNAME="augustineteri157@gmail.com"
+export MAIL_PASSWORD='34850829amk'
+
+python3 manage.py server
