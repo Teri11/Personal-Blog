16be7c9258c56580a8df1da64fc42906b69519b2
Teri11 <augustineteri157@gmail.com>
2022-03-13 13:36:18 +0300

    will edit flask  importation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm 
 from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
